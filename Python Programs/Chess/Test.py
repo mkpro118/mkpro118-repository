f81b969c7530648d27e116ac0b944e3d41daf206
M K <85588877+mkpro118@users.noreply.github.com>
2021-06-20 21:03:35 +0530

    Update Test.py

--- Python Programs/Chess/Test.py
+++ Python Programs/Chess/Test.py
@@ -1,65 +1,32 @@
-from time import sleep
 from Board import Board
 import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('test.log')
+file_handler = logging.FileHandler('test2.log')
 formatter = logging.Formatter('%(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 board = Board()
-board = board.board
 
-# board_ = ''
-# for row in board:
-#     for col in row:
-#         x = f'{col.get_current_position()}' if not isinstance(col, (str, np.ndarray)) else f'{col}'
-#         board_ += x + ' | '
-#     board_ += '\n' + '-' * 63
-#     board_ += '\n'
 
-# print(board_)
+def moves():
+    k = ['d2', 'd4', 'd7', 'd5', 'e2', 'e4', 'd5', 'e4',
+         'f2', 'f3', 'e4', 'f3', 'd1', 'f3', 'd8', 'd4',
+         'b1', 'c3', 'g8', 'f6', 'c1', 'e3', 'd4', 'b4',
+         'a1', 'd1', 'c8', 'g4', 'c3', 'b5', 'g4', 'f3',
+         'b5', 'c7']
+    yield from k
 
-# print(k.is_sliding_piece)
-# board[6, 0] = Empty('a2')
 
-# Moving the e2 Pawn to e4
-k = board[6, 4]
-k.move(board, 'e4')
-logger.info(board)
-logger.info('')
+move = moves()
 
-sleep(1.5)
-
-# Moving the f7 Pawn to f6
-k = board[1, 5]
-k.move(board, 'f6')
-logger.info(board)
-logger.info('')
-
-sleep(1.5)
-
-# Moving the d2 Pawn to d4
-k = board[6, 3]
-k.move(board, 'd4')
-logger.info(board)
-logger.info('')
-
-sleep(1.5)
-
-# Moving the g7 Pawn to g5
-k = board[1, 6]
-k.move(board, 'g5')
-logger.info(board)
-logger.info('')
-
-sleep(1.5)
-
-# Moving the d1 Queen to h5
-k = board[7, 3]
-k.move(board, 'h5')
-
-logger.info(board)
-logger.info('')
+while True:
+    try:
+        k = board.getPiece(next(move))
+        k.move(board.board, next(move))
+        logger.info(board)
+        logger.info('')
+    except StopIteration:
+        break
