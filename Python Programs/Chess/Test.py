88f6f7d087ae1f9f9bd42a03d4ee5feacb3a75e6
M K <85588877+mkpro118@users.noreply.github.com>
2021-06-21 03:02:59 +0530

    Update Test.py

--- Python Programs/Chess/Test.py
+++ Python Programs/Chess/Test.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('test2.log')
+file_handler = logging.FileHandler('test.log')
 formatter = logging.Formatter('%(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -12,11 +12,14 @@ board = Board()
 
 
 def moves():
-    k = ['d2', 'd4', 'd7', 'd5', 'e2', 'e4', 'd5', 'e4',
-         'f2', 'f3', 'e4', 'f3', 'd1', 'f3', 'd8', 'd4',
-         'b1', 'c3', 'g8', 'f6', 'c1', 'e3', 'd4', 'b4',
-         'a1', 'd1', 'c8', 'g4', 'c3', 'b5', 'g4', 'f3',
-         'b5', 'c7']
+    k = ['e2', 'e4', 'e7', 'e5', 'b1', 'c3', 'b8', 'c6',
+         'f1', 'c4', 'f8', 'c5', 'd1', 'g4', 'd8', 'f6',
+         'c3', 'd5', 'f6', 'f2', 'e1', 'd1', 'g7', 'g6',
+         'g1', 'h3', 'f2', 'd4', 'd2', 'd3', 'h7', 'h5',
+         'g4', 'f3', 'g8', 'f6', 'c2', 'c3', 'f6', 'd5',
+         'c3', 'd4', 'e5', 'd4', 'e4', 'd5', 'c6', 'a5',
+         'h1', 'e1', 'e8', 'f8', 'e1', 'f1', 'a5', 'c4',
+         'f3', 'f7']
     yield from k
 
 
