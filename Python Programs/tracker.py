1da5b236fb7701f79bbf237af0c4568d9a21bc77
M K <85588877+mkpro118@users.noreply.github.com>
2021-08-18 01:42:34 +0530

    Update tracker.py

--- Python Programs/tracker.py
+++ Python Programs/tracker.py
@@ -1,6 +1,7 @@
 from time import perf_counter as pc
 from os import getpid
 from psutil import Process
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 def cur_mem():
@@ -11,7 +12,7 @@ def track_time(func):
     def wrapper(*args, **kwargs):
         start = pc()
         result = func(*args, **kwargs)
-        print(f'\nTime Taken by {func.__name__}:\t\t {pc() - start} seconds\n')
+        print(f'\nTime Taken by {func.__name__}:\t\t {pc() - start} seconds')
         return result
     return wrapper
 
@@ -20,7 +21,7 @@ def track_memory(func):
     def wrapper(*args, **kwargs):
         start = cur_mem()
         result = func(*args, **kwargs)
-        print(f'\nMemory Used by {func.__name__}:\t\t {cur_mem() - start} Mb\n')
+        print(f'\n\nMemory Used by {func.__name__}:\t\t {cur_mem() - start} Mb')
         return result
     return wrapper
 
@@ -30,8 +31,8 @@ def track_memory_and_time(func):
         start_memory = cur_mem()
         start_time = pc()
         result = func(*args, **kwargs)
-        print(f'\nMemory Used by {func.__name__}:\t\t {cur_mem() - start_memory} Mb\n')
-        print(f'\nTime Taken by {func.__name__}:\t\t {pc() - start_time} seconds\n')
+        print(f'\nMemory Used by {func.__name__}:\t\t {cur_mem() - start_memory} Mb')
+        print(f'\nTime Taken by {func.__name__}:\t\t {pc() - start_time} seconds')
         return result
     return wrapper
 
@@ -42,8 +43,36 @@ def method_call_counter(func):
     '''
     def wrapper(*args, **kwargs):
         wrapper._counter += 1
-        wrapper.counter = f'Function {func.__qualname__} was called:\t\t {wrapper._counter} times'
+        wrapper.counter = f'\nFunction {func.__qualname__} was called:\t\t {wrapper._counter} times'
         return func(*args, **kwargs)
     wrapper._counter = 0
     wrapper.counter = ''
     return wrapper
+
+
+def track_total_time(func):
+    def wrapper(*args, **kwargs):
+        start = pc()
+        result = func(*args, **kwargs)
+        end = pc()
+        wrapper.total_time += end - start
+        wrapper.total_time_list.append(end - start)
+        return result
+    wrapper.total_time = 0
+    wrapper.total_time_list = []
+    return wrapper
+
+
+def multiply_threads(*args, **kwargs):
+    threads = int(kwargs.get('threads', 10))
+
+    def inner(func):
+        def wrapper(*args, **kwargs):
+            with ThreadPoolExecutor() as executor:
+                _ = [executor.submit(func, i) for i in range(1, threads + 1)]
+                for i in as_completed(_):
+                    a, b = i.result()
+                    print(a, b, sep='\n')
+            return
+        return wrapper
+    return inner
