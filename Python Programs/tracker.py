584e6d330988e46c4852f36ab7b25bfbe8eb57a2
M K <85588877+mkpro118@users.noreply.github.com>
2021-08-18 10:07:47 +0530

    Update tracker.py

--- Python Programs/tracker.py
+++ Python Programs/tracker.py
@@ -2,6 +2,7 @@ from time import perf_counter as pc
 from os import getpid
 from psutil import Process
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from functools import wraps
 
 
 def cur_mem():
@@ -9,31 +10,50 @@ def cur_mem():
 
 
 def track_time(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start = pc()
-        result = func(*args, **kwargs)
-        print(f'\nTime Taken by {func.__name__}:\t\t {pc() - start} seconds')
-        return result
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+        else:
+            return result
+        finally:
+            print(f'\nTime Taken by {func.__name__}:\t\t {pc() - start} seconds')
     return wrapper
 
 
 def track_memory(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start = cur_mem()
-        result = func(*args, **kwargs)
-        print(f'\n\nMemory Used by {func.__name__}:\t\t {cur_mem() - start} Mb')
-        return result
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+        else:
+            return result
+        finally:
+            print(f'\n\nMemory Used by {func.__name__}:\t\t {cur_mem() - start} Mb')
     return wrapper
 
 
 def track_memory_and_time(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start_memory = cur_mem()
         start_time = pc()
-        result = func(*args, **kwargs)
-        print(f'\nMemory Used by {func.__name__}:\t\t {cur_mem() - start_memory} Mb')
-        print(f'\nTime Taken by {func.__name__}:\t\t {pc() - start_time} seconds')
-        return result
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+        else:
+            return result
+        finally:
+            print(f'\nMemory Used by {func.__name__}:\t\t {cur_mem() - start_memory} Mb')
+            print(f'\nTime Taken by {func.__name__}:\t\t {pc() - start_time} seconds')
+
     return wrapper
 
 
@@ -41,23 +61,38 @@ def method_call_counter(func):
     '''
     The counter can be accessed using func.counter
     '''
+    @wraps(func)
     def wrapper(*args, **kwargs):
         wrapper._counter += 1
         wrapper.counter = f'\nFunction {func.__qualname__} was called:\t\t {wrapper._counter} times'
-        return func(*args, **kwargs)
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            raise e
+        else:
+            return result
+        finally:
+            pass
     wrapper._counter = 0
     wrapper.counter = ''
     return wrapper
 
 
 def track_total_time(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start = pc()
-        result = func(*args, **kwargs)
-        end = pc()
-        wrapper.total_time += end - start
-        wrapper.total_time_list.append(end - start)
-        return result
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+        else:
+            return result
+        finally:
+            end = pc()
+            wrapper.total_time += end - start
+            wrapper.total_time_list.append(end - start)
+
     wrapper.total_time = 0
     wrapper.total_time_list = []
     return wrapper
@@ -67,12 +102,16 @@ def multiply_threads(*args, **kwargs):
     threads = int(kwargs.get('threads', 10))
 
     def inner(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             with ThreadPoolExecutor() as executor:
-                _ = [executor.submit(func, i) for i in range(1, threads + 1)]
-                for i in as_completed(_):
-                    a, b = i.result()
-                    print(a, b, sep='\n')
+                try:
+                    _ = [executor.submit(func, i) for i in range(1, threads + 1)]
+                    for i in as_completed(_):
+                        a, b = i.result()
+                        print(a, b, sep='\n')
+                except Exception as e:
+                    raise e
             return
         return wrapper
     return inner
