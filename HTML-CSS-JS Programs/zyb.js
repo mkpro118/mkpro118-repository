0fcffb3c535ef7018e65541258df8ba67b0d7c03
M K <85588877+mkpro118@users.noreply.github.com>
2022-04-12 08:35:45 +0530

    Update zyb.js

--- HTML-CSS-JS Programs/zyb.js
+++ HTML-CSS-JS Programs/zyb.js
@@ -14,13 +14,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms)); }
 
 async function cheat() {
-    click_speed_control()
-    click_start_button()
-    for (let i=0; i < 5; i++) {
-        click_play_button()
-        await sleep(5000)
-    }
-
     document.querySelectorAll('div.question-container').forEach(async q => {
         try {
             q.querySelector('button.show-answer-button').click()
@@ -30,4 +23,11 @@ async function cheat() {
             q.querySelector('textarea').value = q.parentNode.nextElementSibling.querySelector('span.forfeit-answer').innerText.trim()
         } catch(err) {}
     })
+
+    click_speed_control()
+    click_start_button()
+    for (let i=0; i < 5; i++) {
+        click_play_button()
+        await sleep(5000)
+    }
 }
