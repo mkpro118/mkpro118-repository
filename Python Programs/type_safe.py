0db50b0615337daed3c95c7697b077b6659f8e43
M K <85588877+mkpro118@users.noreply.github.com>
2022-07-01 05:05:23 +0530

    Added Documentation!

--- Python Programs/type_safe.py
+++ Python Programs/type_safe.py
@@ -2,15 +2,28 @@ from functools import wraps
 from inspect import getfullargspec
 
 
-def type_safe(func):
+def type_safe(func: callable):
+    '''
+    This decorator ensures type safety over type annotated functions
+    '''
+    # Argument inspection
     specs = getfullargspec(func)
-    annotations = specs.annotations
-    annotations = annotations.get
+    
+    # Function to get items from the annotation dictionary
+    # annotations is actually the callable `get` method of a dict object
+    annotations = specs.annotations.get
 
+    # Keyword argument filter
     kw_filter = lambda y: lambda x: x not in y
 
-    def validate_types(*args):
+    def validate_types(*args: tuple[tuple]):
+        '''
+        Validates the types from the tuples in args
+        '''
+        # List of type errors, to show all errors at once
         errors = []
+        
+        # Iterating over args and validating each component
         for req, real in args:
             try:
                 assert isinstance(real, annotations(req, object))
@@ -20,11 +33,15 @@ def type_safe(func):
                     f"for parameter '{req}'",
                     f", got {type(real)} instead!",
                 ))
+
         if len(errors) > 0:
+            # Error Message formatting
             max_l = [max(map(lambda x: len(x[i]), errors)) for i in range(3)]
             r = lambda x, i: x + ' ' * (max_l[i] - len(x))
             errors = map(lambda x: (r(x[0], 0), r(x[1], 1), r(x[2], 2),), errors)
             errors = '\n'.join(map(lambda x: ' '.join(x), errors))
+            
+            # Raising a TypeError with the formatted error message
             raise TypeError(
                 f'Function <{func.__qualname__}> invoked with incorrect types\n'
                 f'{"-" * (sum(max_l) + 2)}\n'
@@ -34,15 +51,23 @@ def type_safe(func):
 
     @wraps(func)
     def wrapper(*args, **kwargs):
+        '''
+        Wrapper Function over `func` to provide type safety checks
+        '''
         # Validate the positional arguments
         validate_types(*zip(filter(kw_filter(kwargs), specs.args), args))
 
         # Validate the keyword argument
         validate_types(kwargs.items())
 
+        # Calculate the result of func
         result = func(*args, **kwargs)
+        
+        # Validate the return type
         validate_types(('return', result, ))
 
+        # Return the result
         return result
 
+    # Return wrapper function
     return wrapper
